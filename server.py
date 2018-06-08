aa8277a78f9c9d867d6e9014a30974c1f88f19db
Katie Vinton <kvinton13@gmail.com>
2018-06-07 21:02:30 -0700

    Adds route to calculate off season progression

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ from flask import (Flask, render_template, redirect, request, flash,
 
 import os
 
+import math 
+
 app = Flask(__name__)
 
 app.secret_key = "ABC"
@@ -14,6 +16,23 @@ app.jinja_env.undefined = StrictUndefined
 ################################################################################
 
 ################### VIEW FUNCTIONS RENDERING TEMPLATES #########################
+
+def calculate_1_rep_max(weight, reps):
+
+    return (weight * reps * 0.033) + weight
+
+def calculate_working_weight(target_max, reps):
+
+    return target_max/((reps * 0.033) + 1)
+
+
+def myround(x, prec=2, base=2.5):
+  return round(base * round(float(x)/base),prec)
+
+
+def roundup(x):
+    return int(math.ceil(x / 2.5)) * 2.5
+
 
 @app.route('/')
 def index():
@@ -30,12 +49,12 @@ def calculate_rep_max():
     weight = float(weight)
     reps = float(reps)
 
-    max_weight = (weight * reps * 0.033) + weight
+    max_weight = calculate_1_rep_max(weight, reps)
 
     return str(round(max_weight,2))
 
 @app.route('/calculate-working-weight')
-def calculate_working_weight():
+def calculate_weight():
 
     target_max = request.args.get('target-max')
     reps = request.args.get('reps')
@@ -45,20 +64,66 @@ def calculate_working_weight():
     target_max = float(target_max)
     reps = float(reps)
 
-    weight = target_max/((reps * 0.033) + 1)
-
-
+    weight = calculate_working_weight(target_max, reps)
 
     print(weight)
 
     return str(round(weight,2))
+
+@app.route("/off-season")
+def render_off_season():
+
+    return render_template('offseason.html')
+
+@app.route('/calculate-next-rep-max')
+def calculate_next_max():
+
+    weight = request.args.get('weight')
+    reps = request.args.get('reps')
+    week = request.args.get('week')
+
+    print week
+
+    weight = float(weight)
+    reps = float(reps)
+    week = int(week)
+
+    max_weight = calculate_1_rep_max(weight, reps)
+
+    rep_scheme = [8, 5, 8, 5, 3, 5, 3, 2]
+
+    rep_scheme = rep_scheme[week:]
+
+    upcoming_weights = []
+
+    for reps in rep_scheme:
+
+        max_weight = max_weight + 1
+
+        new_weight = calculate_working_weight(max_weight, reps)
+
+        if new_weight % 2.5 != 0:
+            new_weight = roundup(new_weight)
+            max_weight = calculate_1_rep_max(new_weight, reps)
+            print max_weight
+
+
+        upcoming_weights.append((round(new_weight,2), reps, round(max_weight,2)))
+
+    
+    my_dict = {'weights': upcoming_weights}
+
+    return jsonify(my_dict)
+
+
+
 
 
 
 
 if __name__ == "__main__":
 
-    app.debug = False
+    app.debug = True
 
     port = int(os.environ.get('PORT', 5000))
 
---
+++ static/offseason.js
@@ -0,0 +1,28 @@
+function sendData (evt) {
+    evt.preventDefault()
+
+    weight = $('#weight').val()
+    reps = $('#reps').val()
+    week = $('#week').val()
+
+    console.log(week)
+
+    let formInputs = {'weight': weight, 'reps': reps, 'week': week}
+
+    $.get('/calculate-next-rep-max', formInputs, displayRepMax)
+}
+
+function displayRepMax (response) {
+
+    $('#plan').html('')
+
+    for (let plan of response.weights) {
+        $('#plan').append(`<b>Reps:</b> ${plan[1]}   <b>Weight:</b> ${plan[0]}   <b>New Max:</b> ${plan[2]}<br>`)
+    }
+
+    
+}
+
+
+$('#next-max').on('submit', sendData)
+
